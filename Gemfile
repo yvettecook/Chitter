557013bd661c97260fb0415d618c92f92939e39b
yvettecook <yvettecook@gmail.com>
2014-10-12 17:43:08 +0100

    datamapper_setup pointed at postgres database

--- Gemfile
+++ Gemfile
@@ -1,11 +1,14 @@
 source 'http://rubygems.org'
 
+ruby '2.0.0'
+
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'bcrypt'
 gem 'rack-flash3'
 gem 'sinatra-partial'
+gem 'pg'
 
 
 group :development, :test do
--- lib/helpers/datamapper_setup.rb
+++ lib/helpers/datamapper_setup.rb
@@ -1,5 +1,9 @@
+require './lib/models/peep'
+require './lib/models/user'
+
+
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+DataMapper.setup(:default, "postgres://misgzmfbcbnqnk:hs0yDyRU7Nx79OVlIJNWY7G3hO@ec2-54-243-44-191.compute-1.amazonaws.com:5432/d9l2v6bktkupdn")
 DataMapper.finalize
 DataMapper.auto_upgrade!
