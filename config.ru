7ada08ea6f91f8c738d36086afa5fbde453a0e42
yvettecook <yvettecook@gmail.com>
2014-10-27 13:32:00 +0000

    Fixed public folder and tested with rackup

--- config.ru
+++ config.ru
@@ -3,3 +3,5 @@ require 'rubygems'
 
 
 run Sinatra::Application
+
+
--- lib/router.rb
+++ lib/router.rb
@@ -3,18 +3,21 @@ require 'data_mapper'
 require 'rack-flash'
 require 'sinatra/partial'
 require 'json'
-set :partial_template_engine, :erb
+require './lib/helpers/helper'
+require './lib/helpers/datamapper_setup'
 
 require_relative 'models/user'
 require_relative 'models/peep'
 
-require './lib/helpers/helper'
-require './lib/helpers/datamapper_setup'
+set :partial_template_engine, :erb
 
 enable :sessions
 set :session_secret, 'secret'
 use Rack::Flash
-set :public_folder, File.dirname(__FILE__) + "./public"
+
+set :public_dir, Proc.new { File.join(root, "public")}
+set :public_folder, 'public'
+
 require_relative 'controllers/sign_in_and_out_controller'
 require_relative 'controllers/new_peep_controller'
 require_relative 'controllers/homepage_controller'
